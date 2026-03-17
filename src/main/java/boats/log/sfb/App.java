5d0e651570abf2a845d854fe548a1722740b2d7c
Andrei Iatsuk <5247416+iatsuk@users.noreply.github.com>
2026-03-17 22:06:52 +0100

    [refactor] Migrate to Spring Context

--- src/main/java/boats/log/sfb/App.java
+++ src/main/java/boats/log/sfb/App.java
@@ -1,29 +1,13 @@
 package boats.log.sfb;
 
-import boats.log.sfb.telegram.TelegramBot;
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-
-import java.io.InputStream;
-import java.util.Properties;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        Properties props = new Properties();
-        try (InputStream in = App.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (in == null) throw new IllegalStateException("application.properties not found on classpath");
-            props.load(in);
-        }
-        final String TOKEN = props.getProperty("telegram.bot.token");
-        if (TOKEN == null || TOKEN.isBlank()) throw new IllegalStateException("telegram.bot.token is not set");
-
-        System.out.println("Starting bot...");
-        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
-            var session = botsApplication.registerBot(TOKEN, new TelegramBot(TOKEN));
-            System.out.println("Bot started: " + session.isRunning());
+    @SuppressWarnings("unused")
+    public static void main(String[] args) throws InterruptedException {
+        try (var ctx = new AnnotationConfigApplicationContext(AppConfig.class)) {
             Thread.currentThread().join();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
--- src/main/java/boats/log/sfb/telegram/TelegramBot.java
+++ src/main/java/boats/log/sfb/telegram/TelegramBot.java
@@ -1,5 +1,7 @@
 package boats.log.sfb.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -7,10 +9,11 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+@Component
 public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
     private final TelegramClient client;
 
-    public TelegramBot(String token) {
+    public TelegramBot(@Value("${telegram.bot.token}") String token) {
         this.client = new OkHttpTelegramClient(token);
     }
 
